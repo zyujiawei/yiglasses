0533ac26f6e21f552dd6c85f9098d785ae4c7b6f
Arthur <zyujiawei@hotmail.com>
2016-04-29 23:33:02 +0800

    index slider & redo design

--- routes/views/sendmail.js
+++ routes/views/sendmail.js
@@ -4,17 +4,7 @@ var nodemailer = require("nodemailer");
 exports = module.exports = function(req, res) {
 	
 	console.log(req.body);
-	
-	// create reusable transporter object using the default SMTP transport
-	// var transporter = nodemailer.createTransport("SMTP",{
-	//   host: "smtp.mxhichina.com", // 主机
-	//   secure: true, // 使用 SSL
-	//   port: 465, // SMTP 端口
-	//   auth: {
-	//     user: "customerservice@yiglasses.com", // 账号
-	//     pass: "Tianyi2015" // 密码
-	//   }
-	// });
+
 	
 	var transporter = nodemailer.createTransport('smtps://customerservice%40yiglasses.com:Kefu2015@smtp.mxhichina.com');
 	// setup e-mail data with unicode symbols
